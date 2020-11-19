6a3d4add1fd948a6c2029c0a1e2d8da2c85be95e
Martina <martina.pozzerle@libero.it>
2020-11-19 15:40:13 +0100

    #implementazione colore, keyboard

--- main.js
+++ main.js
@@ -2,15 +2,27 @@ const myList = new Vue({
   el: "#root",
   data: {
     itemList: [],
+    actives: [],
     inputItem: "",
 
   },
   methods: {
     takeInput: function(){
-      this.itemList.push(this.inputItem)
+      if(this.inputItem == ""){
+        this.itemList.push("--VUOTO--")
+      }else{
+      this.itemList.push(this.inputItem);
+      this.inputItem = "";
+      this.actives.push(false)
+      }
     },
     removeItem: function(index){
       this.itemList.splice(index, 1)
+      this.actives.splice(index, 1)
+    },
+    activate: function(index){
+      const prev = this.actives[index];
+      this.$set(this.actives, index, ! prev)
     }
   }
 })
